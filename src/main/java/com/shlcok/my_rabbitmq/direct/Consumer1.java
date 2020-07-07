4094cb36bf5dbf9b7c9bd5ba679a1a87b788e313
thelostsheep <1776629130@qq.com>
2020-07-07 23:24:31 +0800

    创建完第四个模型 订阅消息模型-定向 一个生产者 一个交换机 多个个消费者

---
+++ src/main/java/com/shlcok/my_rabbitmq/direct/Consumer1.java
@@ -0,0 +1,31 @@
+package com.shlcok.my_rabbitmq.direct;/*
+    @author Shlock
+    @create date 2020-07-07 21:24    
+*/
+
+import com.rabbitmq.client.*;
+import com.shlcok.my_rabbitmq.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer1 {
+    private final static String QUEUE_NAME = "direct_exchange_queue_1";
+    private final static String EXCHANGE_NAME = "direct_exchange_test";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //将队列绑定交换机
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "update");
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "delete");
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("body = " + new String(body));
+            }
+        };
+        channel.basicConsume(QUEUE_NAME,true, defaultConsumer);
+    }
+}
